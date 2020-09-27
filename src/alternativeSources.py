d1d26d8ca48f21851cf86d154960294c7aaf8a84
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-09-27 13:29:02 +0200

    add todo

--- src/alternativeSources.py
+++ src/alternativeSources.py
@@ -6,6 +6,7 @@ LIMIT = 10
 
 def googleSearch(title, limit=LIMIT):
     urls = []
+    # TODO: add title to return for better display
     for url in search(title, stop=limit):
         urls.append(url)
     return urls
