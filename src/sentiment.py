c668ae9b60532697ad96b7a45fc7d1fb2326ca03
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-09-27 13:31:21 +0200

    sentiment rework

--- src/sentiment.py
+++ src/sentiment.py
@@ -8,12 +8,13 @@ class Sentiment:
     docstring
     """
 
-    def __init__(self):
+    def __init__(self, processor):
         self.model = SentimentModel()
+        self.processor = processor
 
-    def getArticleSentiment(self, article):
+    def getArticleSentiment(self, sentences):
         # use get sentences method
-        sentences = article.split(".")
+        # sentences = article.split(".")
         sentences = prepareText(sentences)
         result = self.model.predict_sentiment(sentences)
         articleSentimenDict = dict(Counter(result))
