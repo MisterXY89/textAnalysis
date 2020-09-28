c918b4fa0b7b0c1ec15a8c5477084ead9932c212
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-09-28 17:18:27 +0200

    outsource & refactor config

--- app.py
+++ app.py
@@ -2,87 +2,28 @@
 from gevent import monkey
 monkey.patch_all()
 
-import os
-import sys
-import json
-import time
+from config import *
 
-from config import Config
-
-# import redis
-# from flask_kvsession import KVSessionExtension
-# from simplekv.memory.redisstore import RedisStore
-
-from functools import wraps
-from flask import Flask, request, redirect, url_for, render_template, session, jsonify
-
-config = Config()
-
-collector = config.collector
-processor = config.processor
-sentiment = config.sentiment
-complexity = config.complexity
-googleSearch = config.altSources.googleSearch
-
-app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.secret_key = config.FLASK_SECRET
-
-
-# store = RedisStore(redis.StrictRedis())
-# KVSessionExtension(store, app)
 
 @app.route("/analyse", methods=["POST"])
 def analyse():
-    data = request.form
-    if not "newsURL" in data:
-        return redirect(url_for("error", msg= "Fehlender Parameter *newsURL in analyse POST"))
+	data = request.form
+	if not "newsURL" in data:
+		return redirect(url_for("error", msg= "Fehlender Parameter *newsURL in analyse POST"))
 
-    articleData = collector.collect([data["newsURL"]])[0]
-    maintext = articleData.maintext
-    article = processor.process(maintext, articleData.title)
-    articleComplexity = complexity.fleschReadingEase(maintext)
-    articleSentiment, articleSentimentDict = sentiment.getArticleSentiment(article.sentences)
-    try:
-        altSourcesForArticle = googleSearch(" ".join(article.titleNounPhrases))
-    except Exception as e:
-        print(f"Error for: {article.titleNounPhrases=}")
-        altSourcesForArticle = googleSearch(articleData.title)
+	urls = list(data["newsURL"])
+	article = API.newsAnalysis(urls)
+	session["renderArticle"] = article
 
-    wordcloud = article.wordcloudImage
-
-    renderArticle = {
-        "meta" : {
-            "date_publish" : articleData.date_publish,
-            "date_download" : articleData.date_download,
-            "url" : articleData.url,
-            "image_url" : articleData.image_url,
-            "authors" : articleData.authors
-        },
-        "title" : articleData.title,
-        "maintext" : articleData.maintext,
-        "complexity" : articleComplexity,
-        "comeplexityType" : "fleschReadingEase",
-        "sentiment" : articleSentiment,
-        "sentimentDict" : articleSentimentDict,
-        "totalWords" : len(article.words),
-        "totalSentences" : len(article.sentences),
-        "alternativeSources" : altSourcesForArticle
-        # "wordcloudImageUrl" : article.wordcloudImageUrl
-    }
-
-    session["renderArticle"] = renderArticle
-
-    return redirect(url_for("result", renderArticle="session", wordcloudImageBase64=article.wordcloudImageBase64))
+	return redirect(url_for("result", renderArticle="session"))
 
 @app.route("/result")
 def result():
     data = request.args
     if not "renderArticle" in data:
         return redirect(url_for("error", msg= "Fehlender Parameter *article in result"))
-    wordcloudImageBase64 = str(data["wordcloudImageBase64"])
     article = session["renderArticle"]
-    return render_template("result.html", article=article, wordcloudImageBase64=wordcloudImageBase64)
+    return render_template("result.html", article=article)
 
 @app.route("/error")
 def error():
--- config.py
+++ config.py
@@ -1,35 +1,83 @@
 
 import os
 import time
+import urllib.parse
 from dotenv import load_dotenv
 
-from src import collector
-from src import processor
-from src import sentiment
-from src import complexity
-from src import wikiSearch as wiki
-from src import alternativeSources as altSources
+
+import redis
+from redis import Redis
+from flask_kvsession import KVSessionExtension
+from simplekv.memory.redisstore import RedisStore
+
+from functools import wraps
+from flask import Flask, request, redirect, url_for, render_template, session, jsonify
+
+
 
 load_dotenv(verbose=True)
 
 class Config:
-    def __init__(self):
-        self.FLASK_SECRET = os.getenv("FLASK_SECRET")
-        self.TIMEZONE = 'Europe/Berlin'
-        # some test urls
-        self.urls = [
-            "https://www.sueddeutsche.de/wirtschaft/bahn-milliarden-ausbau-1.5029830",
-            "https://www.bild.de/regional/saarland/saarland-news/corona-verstoesse-in-saarlouis-polizei-macht-shisha-bar-dicht-72886644.bild.html",
-            "https://www.sueddeutsche.de/leben/aktuell-klokultur-1.5035940",
-            "https://www.sueddeutsche.de/sport/schalke-04-wagner-1.5039548"
-        ]
 
-        self.collector = collector.Collector()
-        self.processor = processor.Processor()
-        self.sentiment = sentiment.Sentiment(self.processor)
-        self.complexity = complexity.TextComplexity(self.processor)
-        self.altSources = altSources
+	def __init__(self, importAPI=True):
 
-        # set timezone:
-        os.environ['TZ'] = self.TIMEZONE
-        time.tzset()
+		from src import collector
+		from src import processor
+		from src import sentiment
+		from src import complexity
+		from src import wikiSearch as wiki
+		from src import alternativeSources as altSources
+
+		self.importAPI = importAPI
+
+		# some test urls
+		self.urls = [
+			"https://www.sueddeutsche.de/wirtschaft/bahn-milliarden-ausbau-1.5029830",
+			"https://www.bild.de/regional/saarland/saarland-news/corona-verstoesse-in-saarlouis-polizei-macht-shisha-bar-dicht-72886644.bild.html",
+			"https://www.sueddeutsche.de/leben/aktuell-klokultur-1.5035940",
+			"https://www.sueddeutsche.de/sport/schalke-04-wagner-1.5039548"
+		]
+
+
+		self.collector = collector.Collector()
+		self.processor = processor.Processor()
+		self.sentiment = sentiment.Sentiment(self.processor)
+		self.complexity = complexity.TextComplexity(self.processor)
+		self.altSources = altSources
+
+		if self.importAPI:
+
+			from API import API
+
+			self.API = API(self.collector, self.processor, self.sentiment, self.complexity, self.altSources)
+
+			self.FLASK_SECRET = os.getenv("FLASK_SECRET")
+			self.TIMEZONE = 'Europe/Berlin'
+			self.REDIS_URL = "localhost:5000"
+
+			# set timezone:
+			os.environ['TZ'] = self.TIMEZONE
+			time.tzset()
+
+
+config = Config()
+
+if config.importAPI:
+	collector = config.collector
+	processor = config.processor
+	sentiment = config.sentiment
+	complexity = config.complexity
+	googleSearch = config.altSources.googleSearch
+
+	API = config.API
+
+	app = Flask(__name__)
+	app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+	app.secret_key = config.FLASK_SECRET
+
+	store = RedisStore(redis.StrictRedis())
+	KVSessionExtension(store, app)
+
+	urllib.parse.uses_netloc.append('redis')
+	url = urllib.parse.urlparse(config.REDIS_URL)
+	conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
