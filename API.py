71b3906b25383d90277bdb7b49d24b3afd1b90d5
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-09-30 13:59:04 +0200

    indention once again & first case adaptions

--- API.py
+++ API.py
@@ -1,84 +1,90 @@
+"""
+simple interface to use all
+the web app's functionalities
+
+@version 2020.09
+"""
 
 import validators
 import ApiConfig as cf
 
 class API:
-	"""
-	simple interface to use all
-	the web app's functionalities
-	"""
+    """
+    simple interface to use all
+    the web app's functionalities
+    """
 
-	def __init__(self, config = None):
-		"""
-		constructor:
+    def __init__(self, config = None):
+        """
+        constructor:
 
-		optionally load apiConfig for external (non-flask) API use
-		via the config param
-		"""
-		if config == None:
-			config = cf.ApiConfig()
+        optionally load apiConfig for external (non-flask) API use
+        via the config param
+        """
+        if config == None:
+            config = cf.ApiConfig()
 
-		self.collector = config.collector
-		self.processor = config.processor
-		self.sentiment = config.sentiment
-		self.complexity = config.complexity
-		self.googleSearch = config.altSources.googleSearch
+        self.collector = config.collector
+        self.processor = config.processor
+        self.sentiment = config.sentiment
+        self.complexity = config.complexity
+        self.google_search = config.altSources.google_search
 
-	def _areValidUrls(self, urls):
-		"""
-		verify if list of urls are valid
-		via the validators package
-		"""
-		for url in urls:
-			if not validators.url(url):
-				return False
-		return True
+    def _are_valid_urls(self, urls):
+        """
+        verify if list of urls are valid
+        via the validators package
+        """
+        for url in urls:
+            if not validators.url(url):
+                return False
+        return True
 
-	def newsAnalysis(self, urls, valid=False):
-		"""
-		! currently only the first url is analyzed
+    def news_analysis(self, urls, valid=False):
+        """
+        ! currently only the first url is analyzed
 
-		perform a complete analysis -
-		bringing together all implemented techniques
+        perform a complete analysis -
+        bringing together all implemented techniques
 
-		if you are sure that your url is valid set valid = True
-		-> this option is used for Flask app (url gets checked via JS)
-		"""
-		if not valid: # if urls have already been validated
-			if not self._areValidUrls(urls):
-				return "Please provide a valid URL (list)"
+        if you are sure that your url is valid set valid = True
+        -> this option is used for Flask app (url gets checked via JS)
+        """
+        if not valid: # if urls have already been validated
+            if not self._are_valid_urls(urls):
+                return "Please provide a valid URL (list)"
 
-		# collect data
-		articleData = self.collector.collect(urls)[0]
-		maintext = articleData.maintext
-		article = self.processor.process(maintext, articleData.title)
-		articleComplexity = self.complexity.fleschReadingEase(maintext)
-		articleSentiment, articleSentimentDict = self.sentiment.getArticleSentiment(article.sentences)
-		try:
-			altSourcesForArticle = self.googleSearch(" ".join(article.titleNounPhrases))
-		except Exception as e:
-			print(f"Error for: {article.titleNounPhrases=}")
-			altSourcesForArticle = self.googleSearch(articleData.title)
+        # collect data
+        articleData = self.collector.collect(urls)[0]
+        maintext = articleData.maintext
+        article = self.processor.process(maintext, articleData.title)
+        articleComplexity = self.complexity.fleschReadingEase(maintext)
+        articleSentiment, articleSentimentDict = self.sentiment.getArticleSentiment(article.sentences)
+        try:
+            altSourcesForArticle = self.google_search(" ".join(article.titleNounPhrases))
+        except Exception:
+            print(f"Error for: {article.titleNounPhrases=}")
+            altSourcesForArticle = self.google_search(articleData.title)
 
-		# prepare return dictionary
-		renderArticle = {
-			"meta" : {
-				"date_publish" : articleData.date_publish,
-				"date_download" : articleData.date_download,
-				"url" : articleData.url,
-				"image_url" : articleData.image_url,
-				"authors" : articleData.authors
-			},
-			"title" : articleData.title,
-			"maintext" : articleData.maintext,
-			"complexity" : articleComplexity,
-			"comeplexityType" : "fleschReadingEase",
-			"sentiment" : articleSentiment,
-			"sentimentDict" : articleSentimentDict,
-			"totalWords" : len(article.words),
-			"totalSentences" : len(article.sentences),
-			"alternativeSources" : altSourcesForArticle,
-			# "wordcloudImageUrl" : article.wordcloudImageUrl
-			"wordcloudImageBase64": article.wordcloudImageBase64
-		}
-		return renderArticle
+        # prepare return dictionary
+        renderArticle = {
+            "meta" : {
+                "date_publish" : articleData.date_publish,
+                "date_download" : articleData.date_download,
+                "url" : articleData.url,
+                "image_url" : articleData.image_url,
+                "authors" : articleData.authors
+            },
+            "title" : articleData.title,
+            "maintext" : articleData.maintext,
+            "complexity" : articleComplexity,
+            "comeplexityType" : "fleschReadingEase",
+            "sentiment" : articleSentiment,
+            "sentimentDict" : articleSentimentDict,
+            "totalWords" : len(article.words),
+            "totalSentences" : len(article.sentences),
+            "alternativeSources" : altSourcesForArticle,
+            # "wordcloudImageUrl" : article.wordcloudImageUrl
+            "wordcloudImageBase64": article.wordcloudImageBase64.decode("utf-8")
+        }
+        return renderArticle
