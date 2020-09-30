da19949cc53e25bfb04fea9a4d6663378bb89829
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-09-30 13:00:11 +0200

    switch from md to sm & indent problems

--- src/processor.py
+++ src/processor.py
@@ -2,7 +2,7 @@
 import json
 import spacy
 import requests
-import de_core_news_md
+import de_core_news_sm
 from io import BytesIO
 from base64 import b64encode
 from wordcloud import WordCloud
@@ -12,71 +12,71 @@ from textblob_de.lemmatizers import PatternParserLemmatizer
 from textblob_de import PatternTagger, PatternParserNPExtractor
 
 class Processor:
-    """
-    docstring for Processor
-    """
+	"""
+	docstring for Processor
+	"""
 
-    def __init__(self):
-		self.nlp = de_core_news_md.load()
-        # self.nlp = spacy.load("de_core_news_md")
-        self.result = NewsArticle()
+	def __init__(self):
+		self.nlp = de_core_news_sm.load()
+        # self.nlp = spacy.load("de")
+		self.result = NewsArticle()
 
-    def process(self, maintext, title=""):
-        self.text = maintext
-        self.title = title
-        self._lemmatizer = PatternParserLemmatizer()
-        self.NPExtractor = PatternParserNPExtractor()
-        self.blob = TextBlob(self.text, pos_tagger=PatternTagger(include_punc=True))
-        self.result.parsed = self.blob.parse()
-        self.result.blob = self.blob
-        self.result.sentences = [str(sentence) for sentence in self.blob.sentences]
-        self.result.words = self.blob.words
-        self.result.tokens = self.blob.tokens
-        self.result.alphaTokens = self._makeAlpha(self.result.tokens)
-        self.result.lemmas = self._lemmatizer.lemmatize(" ".join(self.result.alphaTokens))
-        self.result.tokensWithoutStops = self._remStopWords(" ".join(self.result.alphaTokens)).split(" ")
-        self.result.lemmasWithoutStops = self._remStopWords(self.result.lemmas, posLemma=True).split(" ")
-        self.result.szFreeSentences = self._removeSZ(self.result.sentences)
-        self._genWordCloud(self.result.tokensWithoutStops)
-        if title != "":
-            self.titleBlob = TextBlob(self.title)
-            doc = self.nlp(self.title)
-            self.result.titleNounPhrases = [chunk.text for chunk in doc.noun_chunks]
-            if len(self.result.titleNounPhrases) <= 0:
-                self.result.titleNounPhrases = self._remStopWords(" ".join(self._makeAlpha(self.titleBlob.tokens)))
-        return self.result
+	def process(self, maintext, title=""):
+		self.text = maintext
+		self.title = title
+		self._lemmatizer = PatternParserLemmatizer()
+		self.NPExtractor = PatternParserNPExtractor()
+		self.blob = TextBlob(self.text, pos_tagger=PatternTagger(include_punc=True))
+		self.result.parsed = self.blob.parse()
+		self.result.blob = self.blob
+		self.result.sentences = [str(sentence) for sentence in self.blob.sentences]
+		self.result.words = self.blob.words
+		self.result.tokens = self.blob.tokens
+		self.result.alphaTokens = self._makeAlpha(self.result.tokens)
+		self.result.lemmas = self._lemmatizer.lemmatize(" ".join(self.result.alphaTokens))
+		self.result.tokensWithoutStops = self._remStopWords(" ".join(self.result.alphaTokens)).split(" ")
+		self.result.lemmasWithoutStops = self._remStopWords(self.result.lemmas, posLemma=True).split(" ")
+		self.result.szFreeSentences = self._removeSZ(self.result.sentences)
+		self._genWordCloud(self.result.tokensWithoutStops)
+		if title != "":
+		    self.titleBlob = TextBlob(self.title)
+		    doc = self.nlp(self.title)
+		    self.result.titleNounPhrases = [chunk.text for chunk in doc.noun_chunks]
+		    if len(self.result.titleNounPhrases) <= 0:
+		        self.result.titleNounPhrases = self._remStopWords(" ".join(self._makeAlpha(self.titleBlob.tokens)))
+		return self.result
 
-    def _removeSZ(self, sentences):
-        return [sentence.replace("ß", "ss") for sentence in sentences]
+	def _removeSZ(self, sentences):
+	    return [sentence.replace("ß", "ss") for sentence in sentences]
 
-    def _remStopWords(self, text, posLemma=False):
-        if posLemma:
-            lemmas = [el[0] for el in text]
-            text = " ".join(lemmas)
-        doc = self.nlp(text)
-        return " ".join([el.text for el in doc if not el.is_stop])
+	def _remStopWords(self, text, posLemma=False):
+	    if posLemma:
+	        lemmas = [el[0] for el in text]
+	        text = " ".join(lemmas)
+	    doc = self.nlp(text)
+	    return " ".join([el.text for el in doc if not el.is_stop])
 
-    def _makeTokenAlpha(self, token):
-        accaptableChars = ["-", "*"]
-        return "".join([char for char in token if str(char).isalpha() or char in accaptableChars])
+	def _makeTokenAlpha(self, token):
+	    accaptableChars = ["-", "*"]
+	    return "".join([char for char in token if str(char).isalpha() or char in accaptableChars])
 
-    def _makeAlpha(self, tokens):
-        return [self._makeTokenAlpha(token) for token in tokens if str(token).isalpha()]
+	def _makeAlpha(self, tokens):
+	    return [self._makeTokenAlpha(token) for token in tokens if str(token).isalpha()]
 
-    def _genWordCloud(self, tokens):
-        # https://www.file.io/
-        # useable only once
-        # upload to & use link:
-        self.result.wordcloud = WordCloud().generate(" ".join(self.result.tokensWithoutStops))
-        self.result.wordcloudImage = BytesIO()
-        self.result.wordcloud.to_image().save(self.result.wordcloudImage, 'PNG')
-        self.result.wordcloudImage.seek(0)
+	def _genWordCloud(self, tokens):
+	    # https://www.file.io/
+	    # useable only once
+	    # upload to & use link:
+	    self.result.wordcloud = WordCloud().generate(" ".join(self.result.tokensWithoutStops))
+	    self.result.wordcloudImage = BytesIO()
+	    self.result.wordcloud.to_image().save(self.result.wordcloudImage, 'PNG')
+	    self.result.wordcloudImage.seek(0)
 
-        self.result.wordcloudImageBase64 = b64encode(self.result.wordcloudImage.read())
-        # response = requests.post("https://file.io", files={"file": self.result.wordcloudImage})
-        self.result.wordcloudImageUrl = ""
-        # if response.status_code == 200:
-        #     self.result.wordcloudImageUrl = json.loads(response.text)["link"]
+	    self.result.wordcloudImageBase64 = b64encode(self.result.wordcloudImage.read())
+	    # response = requests.post("https://file.io", files={"file": self.result.wordcloudImage})
+	    self.result.wordcloudImageUrl = ""
+	    # if response.status_code == 200:
+	    #     self.result.wordcloudImageUrl = json.loads(response.text)["link"]
 
 
 
